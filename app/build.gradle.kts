8850d2aec1b4caf167a97a4d0666a5f5780ef5f7
Shivam Badhopulu <shivambadhopulu@gmail.com>
2024-07-06 17:21:27 +0530

    Added Authentication FIREBASE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,14 @@ plugins {
     alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin)
     id("kotlin-kapt")
     id("kotlin-android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.safety"
     compileSdk = 34
 
+    defaultConfig {
     defaultConfig {
         applicationId = "com.example.safety"
         minSdk = 23
@@ -53,18 +55,18 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     //Room DB
     implementation(libs.androidx.room.ktx)
+    //noinspection KaptUsageInsteadOfKsp
     kapt (libs.androidx.room.compiler)
     androidTestImplementation(libs.androidx.room.testing)
-
     //Firebase
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
-
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
-
     // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.firebase:firebase-analytics")
 
-}
+//    128188297759-oele4knfv581r7dhk38tjeskcdupn378.apps.googleusercontent.com
+}}
---
+++ app/src/main/java/com/example/safety/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.safety
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.internal.GoogleSignInOptionsExtensionParcelable
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 89
+    private lateinit var googleSignInClient: GoogleSignInClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.defaultwebclientid))
+            .requestEmail()
+            .build()
+        googleSignInClient=GoogleSignIn.getClient(this,gso)
+    }
+
+    fun signIn(view: android.view.View) {
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w("fire89", "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+
+        val auth = FirebaseAuth.getInstance()
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Fire89", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity::class.java))
+                    Log.d("Fire89", "firebaseAuthWithGoogle: ${user?.displayName}")
+                } else {
+                    Log.w("Fire89", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+}
